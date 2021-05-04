7ae7859207a6e57ed88232c568e73f6d83da7c24
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-05-04 14:56:23 +0900

    accept French and Spanish. And refactoring

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -5,14 +5,33 @@ import (
 )
 
 func main() {
-	fmt.Println(Hello("Taro"))
+	fmt.Println(Hello("Taro", ""))
 }
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(lang) + name
+}
+
+// (prefix string)はnamed return value と呼ばれ、zero値が割り当てられる。return を呼び出すだけで済むようになる
+func greetingPrefix(lang string) (prefix string) {
+
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
