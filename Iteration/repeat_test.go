ace8c8aaf430d8cd748e907f76485471ac0175ed
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-05-04 16:11:11 +0900

    繰り返し回数を変更できるよう修正

--- Iteration/repeat_test.go
+++ Iteration/repeat_test.go
@@ -3,16 +3,30 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertCorrectMessage := func(t *testing.T, expected, repeated string) {
+		t.Helper()
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
 	}
+
+	t.Run("repeat 5 count", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectMessage(t, expected, repeated)
+	})
+
+	t.Run("repeat 9 count", func(t *testing.T) {
+		repeated := Repeat("a", 9)
+		expected := "aaaaaaaaa"
+		assertCorrectMessage(t, expected, repeated)
+	})
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
