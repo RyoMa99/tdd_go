9feca43d66eb194a41544eb0236e37f28d6a03e1
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-05-05 16:15:49 +0900

    http.ResponseWriterもio.Writerを実装していることを確認

--- DI/greet.go
+++ DI/greet.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+// func main() {
+// 	Greet(os.Stdout, "Elodie")
+// }
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
