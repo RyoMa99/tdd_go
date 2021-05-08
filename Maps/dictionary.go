8acc73c337f30070ea88cf44e49abf3f078b05ad
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-05-08 18:00:55 +0900

    catch error to add exists words

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -1,15 +1,31 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
 // map は参照型なのでポインタでなくてよい
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrorWordExists
+	default:
+		return err
+	}
+	return nil
 }
 
-var ErrorNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrorNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrorWordExists = DictionaryErr("cannot add word becaouse it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
