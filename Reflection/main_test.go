a8bfd16af314d334b9c6c8a3b16fe285bf6792c6
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-05-15 18:29:25 +0900

    allow func

--- Reflection/main_test.go
+++ Reflection/main_test.go
@@ -17,6 +17,23 @@ type Profile struct {
 
 func TestWalk(t *testing.T) {
 
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
 	t.Run("with channels", func(t *testing.T) {
 		aChannel := make(chan Profile)
 
